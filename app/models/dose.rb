520dc71031a6d09c2ff89af75cf4a5ce1b339df6
Grzegorz Kurzajczyk <grzegorz.kurzajczyk@gmail.com>
2020-11-16 09:18:19 +0100

    ingredients/doses html, css needs more work

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,5 +1,5 @@
 class Dose < ApplicationRecord
-  validates :description, presence: true
+
   validates_uniqueness_of :ingredient, :cocktail, :scope => [:ingredient, :cocktail]
 
   belongs_to :ingredient
